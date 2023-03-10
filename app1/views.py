cac6d8de739f600ccf331dd7d2210a99cce31cf6
kalyani <gandikotakalyani334@gmail.com>
2023-03-10 22:23:07 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def dhoni(request):
+    return HttpResponse('dhoni is the Best finisher')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def Rohit(request):
+    return HttpResponse('Rohit is the Best batsman')
